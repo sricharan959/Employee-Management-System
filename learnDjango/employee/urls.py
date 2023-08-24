241a80861a236fe8b49b56b39e4864a22f3ed61a
Sricharan <sricharanajja@gmail.com>
2023-08-24 18:33:08 +0530

    Completed Project

---
+++ learnDjango/employee/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.blog,name='blog'),
+    path('employee/',views.index,name='employee'),
+    path('create/',views.create,name='create'),
+    path('create/createData/',views.createData,name='createData'),
+    path('delete/<int:id>',views.delete,name='delete'),
+    path('update/<int:id>',views.update,name='update'),
+    path('update/updateData/<int:id>',views.updateData,name='updateData'),
+]
